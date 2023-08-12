50cc71865ba56ff5bdd3ed7fc5d0d583e43f7cee
SylviaWhittle <sylviwhittle@gmail.com>
2023-08-12 18:38:42 +0100

    Add search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -31,8 +31,43 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Use the ? to return the error value on error.
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("Contents of file: \n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     // If success, return unit type ()
     Ok(())
+}
+
+// Note that here we clarify that the lifetime of the return strings will
+// be the lifetime of the contents, and not the query.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // search for the query in the line
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        // Note that the \ at the start here tells rust not
+        // to put a \n at the start of this string literal.
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
